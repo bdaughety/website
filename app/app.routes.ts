4a83582f3cceacf9d49122d0ce25edcfddf76ec8
bdaughety <bdaughety@franklinamerican.com>
2016-07-10 14:32:08 -0500

    completed tutorial through routes

---
+++ app/app.routes.ts
@@ -0,0 +1,30 @@
+import { provideRouter, RouterConfig }  from '@angular/router';
+import { HeroesComponent } from './heroes.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+
+const routes: RouterConfig = [
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: '',
+    redirectTo: '/dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'detail/:id',
+    component: HeroDetailComponent
+  },
+  {
+    path: 'heroes',
+    component: HeroesComponent
+  }
+];
+
+const precompile = [HeroesComponent];
+
+export const APP_ROUTER_PROVIDERS = [
+  provideRouter(routes)
+];
